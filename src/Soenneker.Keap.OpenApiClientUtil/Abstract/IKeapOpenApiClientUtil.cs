3076d404ed906a818a8f6fea1d5da5812dfc3cbf
soenneker <jake@soenneker.com>
2026-08-29 14:17:08 -0500

    Improve public API XML documentation

--- src/Soenneker.Keap.OpenApiClientUtil/Abstract/IKeapOpenApiClientUtil.cs
+++ src/Soenneker.Keap.OpenApiClientUtil/Abstract/IKeapOpenApiClientUtil.cs
@@ -2,7 +2,6 @@ using Soenneker.Keap.OpenApiClient;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-
 namespace Soenneker.Keap.OpenApiClientUtil.Abstract;
 
 /// <summary>
@@ -10,5 +9,10 @@ namespace Soenneker.Keap.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IKeapOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured keap OpenAPI Client used by the Keap OpenAPI Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested keap OpenAPI Client.</returns>
     ValueTask<KeapOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Keap.OpenApiClientUtil/Registrars/KeapOpenApiClientUtilRegistrar.cs
+++ src/Soenneker.Keap.OpenApiClientUtil/Registrars/KeapOpenApiClientUtilRegistrar.cs
@@ -13,6 +13,8 @@ public static class KeapOpenApiClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="KeapOpenApiClientUtil"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddKeapOpenApiClientUtilAsSingleton(this IServiceCollection services)
     {
         services.AddKeapOpenApiHttpClientAsSingleton()
@@ -24,6 +26,8 @@ public static class KeapOpenApiClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="KeapOpenApiClientUtil"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddKeapOpenApiClientUtilAsScoped(this IServiceCollection services)
     {
         services.AddKeapOpenApiHttpClientAsSingleton()
